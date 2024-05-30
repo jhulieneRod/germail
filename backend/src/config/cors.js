766e1be8a936620b8ea43f58b65e570292c573e5
jhulieneRod <jhuliene.rodrigues@gmail.com>
2024-05-30 18:50:57 -0300

    commit

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*, 77.37.69.246, previasite.com, www.previasite.com');
+    res.header('Access-Control-Allow-Origin', '*, 77.37.69.246, previasite.com, www.previasite.com, http://77.37.69.246:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.set('X-Powered-By', 'PHP/7.1.7');// fake, é utilizado para não revelar ao cliente qual a linguagem do servidor
