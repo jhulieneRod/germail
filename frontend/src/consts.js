571307d9a1a4f7eb74f06e6ea8497a50160dbdab
jhulieneRod <jhuliene.rodrigues@gmail.com>
2024-05-30 19:05:44 -0300

    ok 	modified:   frontend/package.json 	modified:   frontend/src/consts.js

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -3,8 +3,8 @@ const consts = {
     // API_URL: 'http://localhost:14105/api',
     // OAPI_URL: 'http://localhost:14105/germail',
 
-    API_URL: 'http://77.37.69.246:14105/api',
-    OAPI_URL: 'http://77.37.69.246:14105/germail',
+    API_URL: '/api',
+    OAPI_URL: '/germail',
 
     VERSION:'1.1.05'
 }
